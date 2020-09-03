62c94aae4b173f5ccf4cf5b4b6c4aa7a287423fd
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-09-03 01:13:06 -0300

    CC 001

--- back-end/middlewares/validateJWT.js
+++ back-end/middlewares/validateJWT.js
@@ -5,24 +5,23 @@ const jwtSecret = process.env.JWT_SECRET;
 
 const validateJWT = async (req, _res, next) => {
   const { authorization: token } = req.headers;
-  if (!token) {
-    return next({ error: true, message: 'Token not found', code: 'invalid_data' });
-  }
+
+  if (!token) return next({ error: true, message: 'Token not found', code: 'invalid_data' });
+
   try {
     const validToken = jwt.verify(token, jwtSecret);
     const { data: { email } } = validToken;
-    const userExist = await users.findOne({
-      where: { email },
-      attributes: { exclude: ['published', 'updated'] },
-    });
-    if (!userExist) {
-      return next({ error: true, message: 'User does not exist', code: 'invalid_data' });
-    }
+    const userExist = await users.findOne(
+      { where: { email }, attributes: { exclude: ['published', 'updated'] } },
+    );
+
+    if (!userExist) return next({ error: true, message: 'User does not exist', code: 'invalid_data' });
+
     const { dataValues: { password, ...noPass } } = userExist;
     req.user = noPass;
-    next();
+    return next();
   } catch (err) {
-    next({ error: true, message: err.message, code: 'unauthorized' });
+    return next({ error: true, message: err.message, code: 'unauthorized' });
   }
 };
 
